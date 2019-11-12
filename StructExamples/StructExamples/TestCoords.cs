c62e0cb13aa03a4753c7b545cbf50f307d398639
José Miguel <jose.miguel.db.17894@gmail.com>
2019-11-12 19:35:14 +0100

    Struct examples

---
+++ StructExamples/StructExamples/TestCoords.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructExamples
+{
+    class TestCoords
+    {
+        static void Main()
+        {
+            // Initialize:   
+            //Coords coords1 = new Coords();
+            Coords coords1;
+            Coords coords2 = new Coords(10, 10);
+
+            // Initialize:
+            coords1.x = 10;
+            coords1.y = 20;
+
+            // Display results:
+            Console.Write("Coords 1: ");
+            Console.WriteLine("x = {0}, y = {1}", coords1.x, coords1.y);
+
+            Console.Write("Coords 2: ");
+            Console.WriteLine("x = {0}, y = {1}", coords2.x, coords2.y);
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+
+    public struct Coords
+    {
+        public int x, y;
+
+        public Coords(int p1, int p2)
+        {
+            x = p1;
+            y = p2;
+        }
+    }
+}
