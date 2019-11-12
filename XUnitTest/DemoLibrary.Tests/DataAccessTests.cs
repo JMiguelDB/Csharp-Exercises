630716ad02039e4d8ed24d4580e3f7e58b77a33b
José Miguel <jose.miguel.db.17894@gmail.com>
2019-11-12 19:38:11 +0100

    XUnit tests examples

---
+++ XUnitTest/DemoLibrary.Tests/DataAccessTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using DemoLibrary;
+using DemoLibrary.Models;
+
+namespace DemoLibrary.Tests
+{
+    public class DataAccessTests
+    {
+        [Fact]
+        public void AddPersonToPeopleList_ShouldWork()
+        {
+            PersonModel newPerson = new PersonModel { FirstName = "Tim", LastName = "Corey" };
+            List<PersonModel> people = new List<PersonModel>();
+
+            DataAccess.AddPersonToPeopleList(people, newPerson);
+
+            Assert.True(people.Count == 1);
+            Assert.Contains<PersonModel>(newPerson, people);
+        }
+
+        [Theory]
+        [InlineData("Tim", "", "LastName")]
+        [InlineData("", "Corey", "FirstName")]
+        public void AddPersonToPeopleList_ShouldFail(string firstName, string lastName, string param)
+        {
+            PersonModel newPerson = new PersonModel { FirstName = firstName, LastName = lastName };
+            List<PersonModel> people = new List<PersonModel>();
+
+            Assert.Throws<ArgumentException>(param, () => DataAccess.AddPersonToPeopleList(people, newPerson));
+        }
+
+        [Fact]
+        public void ConvertModelsToCSV_ShouldWork()
+        {
+            List<string> expected = new List<string>();
+            expected.Add("Tim,Corey");
+
+            PersonModel newPerson = new PersonModel { FirstName = "Tim", LastName = "Corey" };
+            List<PersonModel> people = new List<PersonModel>();
+
+            DataAccess.AddPersonToPeopleList(people, newPerson);
+            List<string> output = DataAccess.ConvertModelsToCSV(people);
+
+            Assert.Equal(output, expected);
+        }
+    }
+}
