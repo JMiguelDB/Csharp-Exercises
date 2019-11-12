a57b87c0a96888f791f8fc6d9140714f2a112517
José Miguel <jose.miguel.db.17894@gmail.com>
2019-11-12 19:29:05 +0100

    Asynchronous examples

---
+++ AsynchronousProgrammingExamples/AsynchronousES/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsynchronousES
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            damageButton.Click += async (o, e) =>
+            {
+                int actualDamage = int.Parse(damageText.Text);
+                int damageResult = await Task.Run(() => CalculateDamage(actualDamage));
+                damageText.Text = damageResult.ToString();
+            };
+        }
+
+        private async void DownloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            var stringData = await _httpClient.GetStringAsync("https://www.dotnetfoundation.org");
+            Console.WriteLine(stringData);
+        }
+
+        private int CalculateDamage(int damage)
+        {
+            return damage - 10;
+        }
+
+        private async void ButtonHtmlMatches_Click(object sender, RoutedEventArgs e)
+        {
+            // Capture the task handle here so we can await the background task later.
+            var getDotNetFoundationHtmlTask = _httpClient.GetStringAsync("https://www.dotnetfoundation.org");
+
+            // Any other work on the UI thread can be done here, such as enabling a Progress Bar.
+            // This is important to do here, before the "await" call, so that the user
+            // sees the progress bar before execution of this method is yielded.
+            progressBarMatches.IsEnabled = true;
+            progressBarMatches.Visibility = Visibility.Visible;
+
+            // The await operator suspends SeeTheDotNets_Click, returning control to its caller.
+            // This is what allows the app to be responsive and not block the UI thread.
+            var html = await getDotNetFoundationHtmlTask;
+            int count = Regex.Matches(html, @"\.NET").Count;
+
+            textHtmlMatches.Text = $"Number of .NETs on dotnetfoundation.org: {count}";
+
+            progressBarMatches.IsEnabled = false;
+            progressBarMatches.Visibility = Visibility.Collapsed;
+        }
+    }
+}
---
+++ AsynchronousProgrammingExamples/AsynchronousProgrammingExamples/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgrammingExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                Coffee cup = Coffee.PourCoffee();
+                Console.WriteLine("coffee is ready");
+                var eggsTask = FryEggsAsync(2);
+                var baconTask = FryBaconAsync(3);
+
+                var allTasks = new List<Task> { eggsTask, baconTask };
+                while (allTasks.Any())
+                {
+                    Task finished = await Task.WhenAny(allTasks);
+                    if (finished == eggsTask)
+                    {
+                        Console.WriteLine("eggs are ready");
+                    }
+                    else if (finished == baconTask)
+                    {
+                        Console.WriteLine("bacon is ready");
+                    }
+                    allTasks.Remove(finished);
+                }
+                Console.WriteLine("Breakfast is ready!");
+            }).GetAwaiter().GetResult();
+            Console.ReadLine();
+        }
+
+        private static Task FryBaconAsync(int v)
+        {
+            Task task = Task.Run(() => Console.WriteLine("Frying {0} bacon...",v));
+            return task;
+        }
+
+        private static Task FryEggsAsync(int v)
+        {
+            Task task = Task.Run(() => Console.WriteLine("Frying {0} eggs...", v));
+            return task;
+        }
+    }
+
+    class Coffee
+    {
+        public static Coffee PourCoffee()
+        {
+            Console.WriteLine("Making coffee...");
+            Thread.Sleep(2000);
+            return new Coffee();
+        }
+    }
+
+    public class Pinger
+    {
+        private readonly Timer _timer;
+        private readonly HttpClient _client;
+
+        public Pinger(HttpClient client)
+        {
+            _client = new HttpClient();
+            _timer = new Timer(Heartbeat, null, 1000, 1000);
+        }
+
+        public void Heartbeat(object state)
+        {
+            // Discard the result
+            _ = DoAsyncPing();
+        }
+
+        private async Task DoAsyncPing()
+        {
+            await _client.GetAsync("http://mybackend/api/ping");
+        }
+    }
+
+}
